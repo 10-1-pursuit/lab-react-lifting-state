afb671bde0d4fe9e8629d305ad366d734fbeaf5b
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-08-03 03:35:31 -0400

    lab complete

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,3 +1,18 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees.jsx";
+
+const Event = ({ event, updateEventAttendance }) => {
+  return (
+    <li key={event.id} className="event">
+      <img src={event.eventImage} alt={event.name} />
+      <h5>
+        {event.name} {event.eventType}
+      </h5>
+      <br />
+      <span>Organized by: {event.organizer} </span>
+      <br />
+      <Attendees event={event} updateEventAttendance={updateEventAttendance} />
+    </li>
+  )
 }
+
+export default Event
