2abf96c72b4977b59d475f8a18f46eefc73008ec
Rubby <rubenslombard@pursuit.org>
2023-08-31 22:36:38 -0400

    last commit

--- src/Components/Attendee.jsx
+++ src/Components/Attendee.jsx
@@ -1,3 +1,27 @@
-export default function Attendee() {
-  return;
+export default function Attendee({event, attendee, updateEventAttendance }) {
+  return (
+       <div key={attendee.id} className="attendee">
+                <p>
+                  <button
+                    className="clickable"
+                    onClick={() => updateEventAttendance(event.id, attendee.id)}
+                  >
+                    Attending:
+                  </button>
+                  <span>{attendee.attendance ? "✅" : "❌"}</span>
+                </p>
+
+                <p>
+                  <span>Note:</span> {attendee.note}
+                </p>
+              </div>
+           
+  )
 }
+       
+     
+    
+    
+    
+  
+
